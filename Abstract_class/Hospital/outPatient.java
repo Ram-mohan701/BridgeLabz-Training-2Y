260ec9b8e1aa8710956c30069998a79c2b80d872
Rammohan <rammohansharma7900@gmail.com>
2025-11-11 13:20:25 +0530

    this is Abstract class

---
+++ Abstract_class/Hospital/outPatient.java
@@ -0,0 +1,30 @@
+package Hospital;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutPatient extends Patient implements MedicalRecord {
+    private double consultationFee;
+    private List<String> records;
+
+    public OutPatient(int patientId, String name, int age, String diagnosis, double consultationFee) {
+        super(patientId, name, age, diagnosis);
+        this.consultationFee = consultationFee;
+        this.records = new ArrayList<>();
+    }
+
+    @Override
+    public double calculateBill() {
+        return consultationFee;
+    }
+
+    @Override
+    public void addRecord(String record) {
+        records.add(record);
+    }
+
+    @Override
+    public List<String> viewRecords() {
+        return records;
+    }
+}
